9661f74b07aaf10cdaac5ebab430b45658b8746f
quangoku <lequan159753@gmail.com>
2026-02-27 11:36:13 +0700

    update notification send to only online users

--- apps/notifications/src/gateway/notification.gateway.ts
+++ apps/notifications/src/gateway/notification.gateway.ts
@@ -12,17 +12,34 @@ export class NotificationsGateway
   @WebSocketServer()
   server: Server;
 
+  private onlineUsers = new Map<number, boolean>();
+
   async handleConnection(client: Socket, ...args: any[]) {
     const userId = client.handshake.auth.userId as number;
-    console.log('userid', userId);
+    if (!this.onlineUsers.get(userId)) {
+      this.onlineUsers.set(userId, true);
+    }
     await client.join(`user_${userId}`);
+    console.log('Client connected:', userId);
+    console.log(this.onlineUsers);
   }
   handleDisconnect(client: Socket) {
-    console.log('Client disconnected:', client.id);
+    const userId = client.handshake.auth.userId as number;
+    if (this.onlineUsers.get(userId) !== undefined) {
+      this.onlineUsers.set(userId, false);
+    }
+    console.log('Client disconnected:', userId);
   }
 
   sendToFollowers(followerIds: any[], data: any) {
-    const rooms = followerIds.map((id) => `user_${id}`);
+    const onlineFollowerIds = followerIds.filter(
+      (id: number) => this.onlineUsers.get(id) === true,
+    );
+    console.log(onlineFollowerIds);
+    if (onlineFollowerIds.length === 0) {
+      return;
+    }
+    const rooms = onlineFollowerIds.map((id) => `user_${id}`);
     this.server.to(rooms).emit(`notification`, data);
     return;
   }
